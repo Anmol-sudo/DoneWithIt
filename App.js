91180fca1994b4cd04fdb4c0a3400f66a344593c
Anmol Sharma <anmolshiv1974@gmail.com>
2024-11-28 09:08:15 +0530

    feat: Implement custom AppPicker component
    - Create reusable AppPicker component for enhanced selection UI
    - Add icon support with MaterialCommunityIcons
    - Implement placeholder text functionality
    - Style component with consistent app design using defaultStyles
    - Ensure component is flexible with spread operator for additional props

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import AppTextInput from "./app/components/AppTextInput";
+import { Switch } from "react-native";
+
 import Screen from "./app/components/Screen";
+import { useState } from "react";
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
+
+  const [isNew, setIsNew] = useState(false);
+
   return (
     <Screen>
-      <AppTextInput placeholder="Username" icon="email" />
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
