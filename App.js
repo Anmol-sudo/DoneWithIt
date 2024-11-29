0026fe74c8bdff9cb76933ad0456e8f9aa35bc54
Anmol Sharma <anmolshiv1974@gmail.com>
2024-11-29 15:23:12 +0530

    feat: Add ImageInput component and handle permissions
    - Create ImageInput component for displaying and capturing images
    - Implement image selection and display functionality
    - Add permission handling for camera and media library access in App.js
    - Style ImageInput component with consistent design
    
    This commit introduces a new reusable ImageInput component and sets up
    the necessary permissions to enable image selection and capture in the app.

--- App.js
+++ App.js
@@ -1,5 +1,39 @@
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+
+import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUrl] = useState();
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) {
+      alert("You need to enable permission to access the library");
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled && result.assets && result.assets.length > 0) {
+        setImageUrl(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput imageUri={imageUri} />
+    </Screen>
+  );
 }
