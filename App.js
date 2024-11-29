2125f74909af4d2eadbc7b6cb4cf6fce200f215c
Anmol Sharma <anmolshiv1974@gmail.com>
2024-11-29 17:01:01 +0530

    Add ImageInputList to ListingEditScreen
    - Integrate ImageInputList component into ListingEditScreen
    - Allow users to add and remove multiple images for listings
    - Enhance form functionality to handle image uploads

--- App.js
+++ App.js
@@ -1,39 +1,5 @@
-import { useEffect, useState } from "react";
-import * as ImagePicker from "expo-image-picker";
-
-import Screen from "./app/components/Screen";
-import { Button, Image } from "react-native";
-import ImageInput from "./app/components/ImageInput";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUri, setImageUrl] = useState();
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted) {
-      alert("You need to enable permission to access the library");
-    }
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.canceled && result.assets && result.assets.length > 0) {
-        setImageUrl(result.assets[0].uri);
-      }
-    } catch (error) {
-      console.log("Error reading an image", error);
-    }
-  };
-
-  return (
-    <Screen>
-      <Button title="Select Image" onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-      <ImageInput imageUri={imageUri} />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
